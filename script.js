e61b79fa92cb3cf1be15a2c0fbfe5021fe4aba44
Akash Shukla <117883959+1707akash@users.noreply.github.com>
2024-01-11 23:52:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,38 @@
 
 let colorDiv = document.getElementById("colorDiv");
 let shapeDiv = document.getElementById("shapeDiv");
+// let body = document.querySelector("body");
 
 let colorBtn = document.getElementById("colorBtn");
 let shapeBtn = document.getElementById("shapeBtn");
+let bgBtn = document.getElementById("bgBtn");
+
+let str= "0123456789abcdef";
 console.log(shapeDiv);
 
 // For color change
 
-colorBtn.addEventListener("click",changeColor);
+colorBtn.addEventListener("click",changeDivColor);
+
+function changeDivColor(){
+    colorDiv.style.backgroundColor= changeColor();
+}
 
 function changeColor(){
-    let str= "0123456789abcdef";
+    
     let hexCode= "#";
     for(let i=0; i<6; i++){
         let randomIndex = Math.floor(Math.random()*str.length);
         hexCode+= str[randomIndex];
     }
+    return hexCode;
     // console.log(hexCode);
-    colorDiv.style.backgroundColor= hexCode;
+    // colorDiv.style.backgroundColor= hexCode;
 }
 
 
 
-// For SHape change 
+// For Shape change 
 
 
 let shapeArr = ["circle","triangle","rhombus","hexagon","pentagon","parallelogram"];
@@ -33,10 +42,12 @@ shapeBtn.addEventListener("click",function(){
     
     // console.log(shapeArr[j]);
     shapeDiv.id = shapeArr[j];
+    shapeDiv.style.backgroundColor = changeColor();
     j++;
     if(j>=shapeArr.length-1){
         j=0;
     }
 });
+
 
 
